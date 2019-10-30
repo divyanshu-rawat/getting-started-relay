c35885a5eb3c7aa8fa2cd7151f0ea0fafc620438
Divyanshu <divyanshu.r46956@gmail.com>
2019-10-30 18:25:09 +0100

    updated server response to be a part of an endpoint.

--- server.js
+++ server.js
@@ -4,16 +4,19 @@ import config from "./config";
 
 const app = express();
 
+let database;
 app.use(express.static("public"));
 
-app.listen(3000);
-
-console.log(config.mongoUrl)
 MongoClient.connect(config.mongoUrl, {useUnifiedTopology: true}, (err, client) => {
     if(err) throw err;
-    const database = client.db('rgr');
+    database = client.db('rgr');
+    app.listen(3000);
+});
+
+
+app.get("/data/links", (req, res) => {
     database.collection("links").find({}).toArray((err, links) => {
         if(err) throw err;
-        client.close();
-    })  
+        res.json(links);
+    });
 });
